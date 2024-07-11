b2be95cdcfd0bd4e9729b5a4076aaa0b8e68bed9
masjeho2 <126146946+masjeho2@users.noreply.github.com>
2024-07-11 13:19:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 pkg update
 pkg install curl
+pkg install wget
+pkg install python
+pkg install pip
+pip install ping3
+
+
 wget https://raw.githubusercontent.com/masjeho2/pingfud/main/ping.sh
 chmod +x ping.sh
 ./ping.sh
